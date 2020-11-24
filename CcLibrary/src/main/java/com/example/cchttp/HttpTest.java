853f8de719498c65fe3a6f4a49f984bc41cd08d8
shengchao.jiang <shengchao.jiang@ecarx.com.cn>
2020-11-24 16:02:26 +0800

    first commit

---
+++ CcLibrary/src/main/java/com/example/cchttp/HttpTest.java
@@ -0,0 +1,12 @@
+package com.example.cchttp;
+
+/**
+ * @author AlienChao
+ * @date 2020/11/24 14:51
+ */
+public class HttpTest {
+
+    public int calculation(int num){
+        return num*2;
+    }
+}
